976edeab6ff3962b1b150eced8435ef70ae0f25e
hejian <9618058+xibufengqing@user.noreply.gitee.com>
2022-05-28 12:54:29 +0800

    trait impl for struct

--- src/example/mod.rs
+++ src/example/mod.rs
@@ -1,3 +1,5 @@
+extern crate regex;
+use super::regex::Regex;
 #[derive(Debug)]
 pub struct Rectangle {
     pub(crate) width: u32,
@@ -17,4 +19,27 @@ impl Rectangle {
     pub fn areas(rectangle: &Rectangle) -> u32{
         rectangle.width * rectangle.height
     }
+}
+
+pub trait Parse {
+    fn parse(s: &Str) -> Self;
+}
+
+impl Parse for u8 {
+    fn parse(s: &Str) -> Self {
+        let re: Regex =Regex::new(r"^[0-9]+]").unwrap();
+        if let Some(captures) = re.captures(s) {
+            captures
+                .get(0)
+                .map_or(0, |s| s.as_str().parse().unwrap_or(0))
+        } else {
+            0
+        }
+    }
+}
+#[test]
+fn parse_should_work() {
+    assert_eq!(u8::parse("123abcd"), 123);
+    assert_eq!(u8::parse("1234abcd"), 0);
+    assert_eq!(u8::parse(abcd), 0);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,36 @@
 mod example;
 use std::fmt;
-use std::io::Write;
-use crate::example::Rectangle;
+use std::fmt::{Arguments, Formatter, write};
+use std::io::{IoSlice, Write};
+use crate::example::{Parse, Rectangle};
 
 struct  BufBuilder {
     buf: Vec<u8>
+}
+
+impl BufBuilder {
+    pub fn new() -> Self {
+        Self {
+            buf: Vec::with_capacity(1024),
+        }
+    }
+}
+
+impl fmt::Debug for BufBuilder {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", String::from_utf8_lossy(&self.buf))
+    }
+}
+
+impl Write for BufBuilder {
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        self.buf.extend_from_slice(buf);
+        Ok(buf.len())
+    }
+
+    fn flush(&mut self) -> std::io::Result<()> {
+        Ok(())
+    }
 }
 
 
@@ -14,5 +40,10 @@ fn main() {
         height: 50,
     };
     println!("The area of the rectangle is {} square pixels ." ,rect1.area());
-    println!("The rectangle has a nonzero width; it is {},", rect1.width)
+    println!("The rectangle has a nonzero width; it is {},", rect1.width);
+
+    let mut buf = BufBuilder::new();
+    buf.write_all(b"Hello world !").unwrap();
+    println!("{:?}",buf);
+    println!("reslut: {}" ,u8::parse("255 hello world"))
 }
