7f765b504e6e18ab2043e5d33b7787524d9ade15
hejian <9618058+xibufengqing@user.noreply.gitee.com>
2022-05-30 18:16:08 +0800

    添加图片的依赖

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,20 @@
-mod example;
-mod structobj;
-
-use std::{fmt, thread};
-use std::fmt::{Arguments, Formatter, write};
-use std::io::{IoSlice, Write};
-use crate::example::{Parse, Rectangle};
-use crate::structobj::Complex;
-
-struct  BufBuilder {
-    buf: Vec<u8>
+struct ImageSpec {
+    specs: Vec<Spec>
 }
 
-impl BufBuilder {
-    pub fn new() -> Self {
-        Self {
-            buf: Vec::with_capacity(1024),
-        }
-    }
+enum Spec {
+    Resize(Resize),
+    Crop(Crop),
 }
 
-impl fmt::Debug for BufBuilder {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", String::from_utf8_lossy(&self.buf))
-    }
+struct Resize {
+    width: u32,
+    height: u32
 }
 
-impl Write for BufBuilder {
-    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
-        self.buf.extend_from_slice(buf);
-        Ok(buf.len())
-    }
+struct Crop {
 
-    fn flush(&mut self) -> std::io::Result<()> {
-        Ok(())
-    }
 }
-
-
 fn main() {
-    let rect1 = Rectangle {
-        width: 30,
-        height: 50,
-    };
-    println!("The area of the rectangle is {} square pixels ." ,rect1.area());
-    println!("The rectangle has a nonzero width; it is {},", rect1.width);
 
-    let mut buf = BufBuilder::new();
-    buf.write_all(b"Hello world !").unwrap();
-    println!("{:?}",buf);
-    println!("reslut: {}" ,u8::parse("255 hello world"));
-
-    let c1 =Complex::new(1.0, 1f64);
-    let c2= Complex::new(2 as f64, 3.0);
-    println!("{:?}",c2+c1);
-
-    let s = String::from("hello world");
-
-    let handle = thread::spawn(move || {
-        println!("moved: {:?}",s);
-
-    });
-
-    handle.join().unwrap();
-
-    // println!("{}",s);
-    println!("moved: {:?}", handle);
 }
